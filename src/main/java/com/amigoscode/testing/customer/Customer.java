51316b02fb05b32ed6200285a431bc6c3112774e
Hüsna POYRAZ <75911392+Husna-POYRAZ@users.noreply.github.com>
2023-06-05 13:43:16 +0300

    added lombok library

--- src/main/java/com/amigoscode/testing/customer/Customer.java
+++ src/main/java/com/amigoscode/testing/customer/Customer.java
@@ -1,6 +1,9 @@
 package com.amigoscode.testing.customer;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +12,9 @@ import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(allowGetters = true)
 public class Customer {
 
@@ -23,38 +29,6 @@ public class Customer {
     @Column(nullable = false, unique = true)
     private String phoneNumber;
 
-    public Customer(UUID id, String name, String phoneNumber) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Customer() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
     @Override
     public String toString() {
